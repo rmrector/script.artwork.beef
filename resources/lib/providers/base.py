04387f98b12ce2572811586b5a419502bd5b2a26
Ryan Rector <rmrector@gmail.com>
2016-01-03 21:30:07 -0700

    Rework default caching to use max-age rather than Expires

--- resources/lib/providers/base.py
+++ resources/lib/providers/base.py
@@ -5,7 +5,7 @@ import xbmcvfs
 from abc import ABCMeta, abstractmethod
 from cachecontrol import CacheControl
 from cachecontrol.caches import FileCache
-from cachecontrol.heuristics import OneDayCache
+from cachecontrol.heuristics import BaseHeuristic
 
 # Result dict of lists, keyed on art type
 # {'url': URL, 'language': ISO alpha-2 code, 'rating': tuple; 0=sortable 1=display, 'size': 0=sortable 1=display, 'provider': self.name}
@@ -28,3 +28,20 @@ class AbstractProvider(object):
     @abstractmethod
     def get_images(self, media_id):
         pass
+
+class OneDayCache(BaseHeuristic):
+    """
+    Cache the response by setting max-age to 1 day.
+    """
+    def update_headers(self, response):
+        headers = {}
+
+        if 'cache-control' not in response.headers or not response.headers['cache-control']:
+            headers['cache-control'] = 'max-age: 86400'
+        elif 'max-age' not in response.headers['cache-control'] and 'no-cache' not in response.headers['cache-control'] and 'no-store' not in response.headers['cache-control']:
+            headers['cache-control'] = response.headers['cache-control'] + ', max-age=86400'
+        return headers
+
+    def warning(self, response):
+        if 'cache-control' not in response.headers or not response.headers['cache-control'] or 'max-age' not in response.headers['cache-control'] and 'no-cache' not in response.headers['cache-control'] and 'no-store' not in response.headers['cache-control']:
+            return '110 - "Response is Stale"'
