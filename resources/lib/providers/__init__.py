a632dd3ec120c2fc6f62a50e0b15ace6d0b861e5
Ryan Rector <rmrector@gmail.com>
2016-01-04 02:27:28 -0700

    Add artwork from TheTVDB

--- resources/lib/providers/__init__.py
+++ resources/lib/providers/__init__.py
@@ -1,6 +1,7 @@
 import mediatypes
 from fanarttv import FanartTvSeriesProvider, FanartTvMovieProvider
+from thetvdbv2 import TheTVDBProvider
 
-_providers = {mediatypes.TVSHOW: (FanartTvSeriesProvider(),), mediatypes.MOVIE: (FanartTvMovieProvider(),)}
+_providers = {mediatypes.TVSHOW: (TheTVDBProvider(), FanartTvSeriesProvider()), mediatypes.MOVIE: (FanartTvMovieProvider(),)}
 def get_providers():
     return _providers
