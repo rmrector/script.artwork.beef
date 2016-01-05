9cae9317a4b775a4c2743578404c684251bc6fa5
Ryan Rector <rmrector@gmail.com>
2016-01-05 00:57:15 -0700

    Get movie artwork from TheMovieDb

--- resources/lib/providers/__init__.py
+++ resources/lib/providers/__init__.py
@@ -1,7 +1,12 @@
 import mediatypes
 from fanarttv import FanartTvSeriesProvider, FanartTvMovieProvider
+from themoviedb import TheMovieDbProvider
 from thetvdbv2 import TheTVDBProvider
 
-_providers = {mediatypes.TVSHOW: (TheTVDBProvider(), FanartTvSeriesProvider()), mediatypes.MOVIE: (FanartTvMovieProvider(),)}
+_providers = {
+    mediatypes.TVSHOW: (TheTVDBProvider(), FanartTvSeriesProvider()),
+    mediatypes.MOVIE: (TheMovieDbProvider(), FanartTvMovieProvider())
+}
+
 def get_providers():
     return _providers
