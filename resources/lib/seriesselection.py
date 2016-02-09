4053514df1c45023cf51a0d8cd11295c33b1375c
Ryan Rector <rmrector@gmail.com>
2016-02-09 00:17:33 -0700

    Better handle cancel and back in series selection

--- resources/lib/seriesselection.py
+++ resources/lib/seriesselection.py
@@ -4,7 +4,8 @@ class SeriesSelector(xbmcgui.WindowXMLDialog):
     def __init__(self, *args, **kwargs):
         super(SeriesSelector, self).__init__(args, kwargs)
         self.serieslist = kwargs.get('serieslist')
-        self.selected = kwargs.get('selected', [])
+        self.originalselected = list(kwargs.get('selected', []))
+        self.selected = []
         self.guilist = None
 
     def prompt(self):
@@ -26,8 +27,9 @@ class SeriesSelector(xbmcgui.WindowXMLDialog):
             art = series['art']
             art['thumb'] = series['art']['poster']
             listitem.setArt(art)
-            if series['imdbnumber'] in self.selected:
+            if series['imdbnumber'] in self.originalselected:
                 listitem.select(True)
+                self.selected.append(series['imdbnumber'])
             self.guilist.addItem(listitem)
         self.setFocus(self.guilist)
 
@@ -43,5 +45,10 @@ class SeriesSelector(xbmcgui.WindowXMLDialog):
         elif controlid == 5:
             self.close()
         elif controlid == 7:
-            self.selected = []
+            self.selected = self.originalselected
+            self.close()
+
+    def onAction(self, action):
+        if action.getId() in (xbmcgui.ACTION_NAV_BACK, xbmcgui.ACTION_PREVIOUS_MENU):
+            self.selected = self.originalselected
             self.close()
