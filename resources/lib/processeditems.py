1a2108d46484cbbf75143e71f27aac10f2e10584
Ryan Rector <rmrector@gmail.com>
2016-02-08 21:46:49 -0700

    Fix up new season detection

--- resources/lib/processeditems.py
+++ resources/lib/processeditems.py
@@ -5,30 +5,33 @@ import xbmc
 from devhelper.pykodi import log
 
 class ProcessedItems(object):
+    version = 1
     def __init__(self, directory):
-        self.tvshow = []
+        self.tvshow = {}
         self.movie = []
         self.episode = []
         self.filename = os.path.join(directory, 'processeditems.json')
 
     def extend(self, extender):
-        self.tvshow.extend(extender.get('tvshow', ()))
+        for tvshowid, seasons in extender.get('tvshow', {}).iteritems():
+            if seasons > self.tvshow.get(tvshowid):
+                self.tvshow[tvshowid] = seasons
         self.movie.extend(extender.get('movie', ()))
         self.episode.extend(extender.get('episode', ()))
 
     def set(self, setter):
-        self.tvshow = setter.get('tvshow', [])
+        self.tvshow = setter.get('tvshow', {})
         self.movie = setter.get('movie', [])
         self.episode = setter.get('episode', [])
 
     def clear(self):
-        self.tvshow = []
+        self.tvshow = {}
         self.movie = []
         self.episode = []
 
     def save(self):
         with open(self.filename, 'w') as jsonfile:
-            json.dump({'tvshow': self.tvshow, 'movie': self.movie, 'episode': self.episode}, jsonfile)
+            json.dump({'tvshow': self.tvshow, 'movie': self.movie, 'episode': self.episode, 'version': self.version}, jsonfile)
 
     def load(self):
         if os.path.exists(self.filename):
@@ -39,6 +42,12 @@ class ProcessedItems(object):
                     self.clear()
                     log("Had trouble loading ProcessedItems, starting fresh.", xbmc.LOGWARNING, tag='json')
                     return
-                self.tvshow = processed.get('tvshow', [])
+
+                if processed.get('version') == self.version:
+                    self.tvshow = dict((int(tvshowid), seasons) for tvshowid, seasons in processed.get('tvshow', {}).iteritems())
+                else:
+                    self.tvshow = {}
                 self.movie = processed.get('movie', [])
                 self.episode = processed.get('episode', [])
+        else:
+            self.clear()
