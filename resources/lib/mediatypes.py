f18f537059c551d6e800adb7d0181d9479e09c77
Ryan Rector <rmrector@gmail.com>
2016-02-15 01:06:05 -0700

    Add settings to select artwork types to automatically download

--- resources/lib/mediatypes.py
+++ resources/lib/mediatypes.py
@@ -1,3 +1,5 @@
+import re
+
 from devhelper import pykodi
 
 TVSHOW = 'tvshow'
@@ -5,124 +7,114 @@ MOVIE = 'movie'
 EPISODE = 'episode'
 SEASON = 'season'
 
-DEFAULT_FANART_LIMIT = 5
+DEFAULT_IMAGE_LIMIT = 5
+
+settings = ('tvshow.poster', 'tvshow.fanart_limit', 'tvshow.banner', 'tvshow.clearlogo', 'tvshow.landscape', 'tvshow.clearart', 'tvshow.characterart', 'season.poster', 'season.banner', 'season.landscape', 'episode.fanart', 'movie.poster', 'movie.fanart_limit', 'movie.banner', 'movie.clearlogo', 'movie.landscape', 'movie.clearart', 'movie.discart')
 
 addon = pykodi.get_main_addon()
 
 artinfo = {
     TVSHOW: {
         'poster': {
-            'localizeid': 32128,
             'autolimit': 1,
             'multiselect': False
 		},
         'fanart': {
-	        'localizeid': 32121,
-	        'autolimit': DEFAULT_FANART_LIMIT,
+	        'autolimit': DEFAULT_IMAGE_LIMIT,
 	        'multiselect': True
 		},
         'banner': {
-	        'localizeid': 32123,
 	        'autolimit': 1,
 	        'multiselect': False
 		},
 		'clearlogo': {
-	        'localizeid': 32126,
 	        'autolimit': 1,
 	        'multiselect': False
 		},
 		'landscape': {
-	        'localizeid': 32130,
 	        'autolimit': 1,
 	        'multiselect': False
 	    },
 		'clearart': {
-	        'localizeid': 32125,
 	        'autolimit': 1,
 	        'multiselect': False
 	    },
 	    'characterart': {
-	        'localizeid': 32127,
 	        'autolimit': 1,
 	        'multiselect': False
 	    }
     },
     MOVIE: {
 		'poster': {
-            'localizeid': 32128,
             'autolimit': 1,
             'multiselect': False
 		},
 		'fanart': {
-	        'localizeid': 32121,
-	        'autolimit': DEFAULT_FANART_LIMIT,
+	        'autolimit': DEFAULT_IMAGE_LIMIT,
 	        'multiselect': True
 		},
 		'banner': {
-	        'localizeid': 32123,
 	        'autolimit': 1,
 	        'multiselect': False
 		},
 		'clearlogo': {
-	        'localizeid': 32126,
 	        'autolimit': 1,
 	        'multiselect': False
 		},
 		'landscape': {
-	        'localizeid': 32130,
 	        'autolimit': 1,
 	        'multiselect': False
 	    },
 		'clearart': {
-	        'localizeid': 32125,
 	        'autolimit': 1,
 	        'multiselect': False
 	    },
 	    'discart': {
-	        'localizeid': 32132,
 	        'autolimit': 1,
 	        'multiselect': False
 	    }
 	},
     SEASON: {
 		'poster': {
-            'localizeid': 32128,
             'autolimit': 1,
             'multiselect': False
 		},
-		# I want season fanart! From press shots or whatever released for/during a season. New characters added, old characters removed, season specific sets or object. Anthology series like 'True Detective' and 'American Horror Story' will have fanart that is nearly always specific to a season.
+		# I want season fanart! From press shots or whatever released for/during a season. New characters added, old characters removed, season specific sets and objects. Anthology series like 'True Detective' and 'American Horror Story' will have fanart that is nearly always specific to a season.
 		# 'fanart': {
         #     'localizeid': 32121,
         #     'autolimit': MULTIPLE_IMAGE_LIMIT,
         #     'multiselect': True
 		# },
 		'banner': {
-	        'localizeid': 32123,
 	        'autolimit': 1,
 	        'multiselect': False
 		},
         'landscape': {
-	        'localizeid': 32130,
 	        'autolimit': 1,
 	        'multiselect': False
 		}
     },
     EPISODE: {
         'fanart': {
-	        'localizeid': 32121,
-	        'autolimit': 1, # Only considered by individual episode or series, not full library
+	        'autolimit': 1,
 	        'multiselect': False
 		}
     }
 }
 
 def update_settings():
-    try:
-        limit = int(addon.get_setting('fanart_limit'))
-    except ValueError:
-        limit = DEFAULT_FANART_LIMIT
-        addon.set_setting('fanart_limit', DEFAULT_FANART_LIMIT)
-    artinfo[TVSHOW]['fanart']['autolimit'] = limit
-    artinfo[MOVIE]['fanart']['autolimit'] = limit
+    for settingid in settings:
+        splitsetting = re.split(r'\.|_', settingid)
+        artinfo[splitsetting[0]][splitsetting[1]]['autolimit'] = get_autolimit(settingid)
+
+def get_autolimit(settingid):
+    result = addon.get_setting(settingid)
+    if settingid.endswith('_limit'):
+        try:
+            return int(result)
+        except ValueError:
+            addon.set_setting(settingid, DEFAULT_IMAGE_LIMIT)
+            return DEFAULT_IMAGE_LIMIT
+    return 1 if result else 0
 
 update_settings()
