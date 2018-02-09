e657c9c355dcc799695a5e5020009b9881890535
Ryan Rector <rmrector@gmail.com>
2018-02-09 01:42:37 -0700

    fix: better selection of episodes to process

--- lib/artworkprocessor.py
+++ lib/artworkprocessor.py
@@ -118,9 +118,11 @@ class ArtworkProcessor(object):
             if mediatype == mediatypes.TVSHOW and not mediatypes.disabled(mediatypes.EPISODE):
                 if mediaitem.uniqueids and any(x in mediaitem.uniqueids.itervalues() for x in settings.autoadd_episodes):
                     medialist.extend(info.MediaItem(ep) for ep in quickjson.get_episodes(dbid))
-                elif settings.generate_episode_thumb:
+                elif not mediatypes.disabled(mediatypes.EPISODE) \
+                and (settings.generate_episode_thumb or settings.download_artwork):
                     for episode in quickjson.get_episodes(dbid):
-                        if not info.has_generated_thumbnail(episode):
+                        if settings.generate_episode_thumb and not info.has_generated_thumbnail(episode) \
+                        or settings.download_artwork and info.has_art_todownload(episode['art']):
                             episode = info.MediaItem(episode)
                             episode.skip_artwork = ['fanart']
                             medialist.append(episode)
