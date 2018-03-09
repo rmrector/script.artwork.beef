96a166f512a2efa92ddf41e0e1bc3ade875bbd82
Ryan Rector <rmrector@gmail.com>
2018-03-08 23:58:56 -0700

    fix: more detail in HTTP error message

--- lib/providers/base.py
+++ lib/providers/base.py
@@ -43,7 +43,8 @@ class AbstractProvider(object):
         except (Timeout, ConnectionError) as ex:
             raise ProviderError, (L(CANT_CONTACT_PROVIDER), ex), sys.exc_info()[2]
         except HTTPError as ex:
-            message = ex.response.reason if ex.response else type(ex).__name__
+            message = ex.response.reason if ex.response else \
+                ex.message if hasattr(ex, 'message') else type(ex).__name__
             raise ProviderError, (L(HTTP_ERROR).format(message), ex), sys.exc_info()[2]
         except RequestException as ex:
             raise ProviderError, (L(HTTP_ERROR).format(type(ex).__name__), ex), sys.exc_info()[2]
